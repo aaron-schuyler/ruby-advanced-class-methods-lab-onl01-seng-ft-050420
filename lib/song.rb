c4f77b5300b29b904bfbb8eeab9f66010d9f2a79
Aaron Schuyler <asky28@gmail.com>
2020-05-17 01:55:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,7 +19,7 @@ class Song
   def self.new_by_name(name)
     self.new(name)
   end
-  def self.new_by_name(name)
+  def self.create_by_name(name)
     new_song = self.new(name)
     @@all << new_song
   end
